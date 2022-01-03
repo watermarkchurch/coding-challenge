456165a394ef9a5584774aa98a59fb5c1a589ceb
Gordon <gburgett@watermark.org>
2022-01-03 10:38:09 -0600

    generators need to be here...

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,10 @@ module CodingChallenge
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |generate|
+      generate.test_framework :rspec
+      generate.factory_bot
+    end
   end
 end
