12002b7cd7bb5a27199b3cc78da23a5b223a61a8
Ryan Gibson <gibsorya@gmail.com>
2022-01-07 16:01:27 -0600

    Added custom title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+    def full_title(title = '')
+        base_title = "Rails Message Board"
+        if title.empty?
+            base_title
+        else
+            title + " | " + base_title
+        end
+    end
 end
