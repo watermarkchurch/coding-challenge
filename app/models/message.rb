9bdccddd0952dc6643f715082dce26b1578046aa
Ryan Gibson <gibsorya@gmail.com>
2022-01-09 19:31:06 -0600

    fixed bug that prevented message deletion

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
-    has_many :taggings
+    has_many :taggings, dependent: :destroy
     has_many :tags, through: :taggings
     has_one_attached :thumbnail
     has_one_attached :video
